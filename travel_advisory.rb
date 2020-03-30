5c299fb4490b55d101e10a37cd7e46c512922432
Marissa <masimons66@gmail.com>
2020-03-30 18:42:48 -0500

    wip

--- travel_advisory.rb
+++ travel_advisory.rb
@@ -16,25 +16,45 @@ response_body = HTTParty.get('https://travel.state.gov/_res/rss/TAsTWs.xml').bod
 hash = XmlSimple.xml_in(response_body.to_s)
 
 res = hash.values[2].first['item'].first['title'].first.split(" - ")
+# redis.set("level 4", [])
 
 hash.values[2].first['item'].each do |el|
-	res = el['title'].first.split(" - ")
+  res = el['title'].first.split(" - ")
+  # if res[1].include?('Level 4')
+  #   old = redis.get("level 4")
+  #   redis.set("level 4", old + [res[0]])
+  # end
   res_hash = { "title" => res[1], "pubDate" => el['pubDate'].first, "link" => el['link'].first }.to_json
 
-	redis.set(res[0].downcase.strip, res_hash)
+  redis.set(res[0].downcase.strip, res_hash)
 end
 
+
 # blah = JSON.parse(redis.get('bhutan'))
-# pp JSON.parse(redis.get('italy'))
+pp JSON.parse(redis.get('italy'))
 
 # pp "#{blah['title']} #{blah['link']}"
 
 # pp hash.values[2].first['item']
 
+########## UPDATE SCRIPT ##########################################################################
+
+# response_body = HTTParty.get('https://travel.state.gov/_res/rss/TAsTWs.xml').body
+
+# hash = XmlSimple.xml_in(response_body.to_s)
+
+# hash.values[2].first['item'].each do |el|
+#   res = el['title'].first.split(" - ")
+#   unless redis.get(res[0]).nil?
+#     value = JSON.parse(redis.get(res[0]))
+#     if res[1] != value
+#   end
+# end
+
 ######### SEND A TEXT MESSAGE ##########################################################################
 
 # account_sid = 'AC7cc8d97bd45ff0bebd419878b45ab98f'
-# auth_token = '592365d069896757c8e19bca846fd293'
+# auth_token = ENV['TWILIO_AUTH_TOKEN']
 # client = Twilio::REST::Client.new(account_sid, auth_token)
 
 # from = '+12059004164' # Your Twilio number
@@ -52,13 +72,13 @@ end
 
 post '/sms-quickstart' do
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
-  	input = params['Body'].downcase.strip
+    input = params['Body'].downcase.strip
     puts input
-  	message = "No information for #{input}"
+    message = "No information for #{input}"
 
-  	unless redis.get(input).nil?
-  		value = JSON.parse(redis.get(input))
-  		message = "#{value['title']}\n\n#{value['link']}"
+    unless redis.get(input).nil?
+      value = JSON.parse(redis.get(input))
+      message = "#{value['title']}\n\n#{value['link']}"
     end
 
     r.message(body: message)
